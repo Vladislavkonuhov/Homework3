85915e686f29f297c6a739c67e7c5de619f485c4
Name <vkonohov19@gmail.com>
2023-06-08 08:54:21 +0300

    update

---
+++ Task1.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+void randomm(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + ", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+randomm(numbers);
+Console.Write($"массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
