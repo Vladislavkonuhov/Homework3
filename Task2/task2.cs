2d456c8cc4318d4bd033ca97dd1c9b3bef62e452
Name <vkonohov19@gmail.com>
2023-06-08 09:14:42 +0300

    update

---
+++ Task2/task2.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] randomArray = new int[a];
+
+void mas(int a)
+{
+    Console.Write("[ ");
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + ", ");
+}
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int kol(int[] randomArray)
+{
+int sum = 0;
+int i = 1;
+while (i < randomArray.Length)
+{
+sum = sum + randomArray[i];
+i = i + 2;
+}
+return sum;
+}
+
+mas(a);
+Console.WriteLine($"\nCумма элементов, стоящих на нечётных позициях: {kol(randomArray)}");
